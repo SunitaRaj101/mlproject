59d94238f91eadf59b629e980e6e85efa44723c8
SunitaRaj101 <sunitafou9@gmail.com>
2023-11-12 07:43:16 +0530

    Data Ingestion-1

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ if __name__=="__main__":
     logging.info("The execution has started")
 
     try:
-        data_ingestion_config=DataIngestionConfig()
+        # data_ingestion_config=DataIngestionConfig()
         data_ingestion=DataIngestion()
-        # data_ingestion.DataIngestion()
+        data_ingestion.initiate_data_ingestion()
         # train_data_path,test_data_path=data_ingestion.initiate_data_ingestion()
 
         # #data_transformation_config=DataTransformationConfig()
